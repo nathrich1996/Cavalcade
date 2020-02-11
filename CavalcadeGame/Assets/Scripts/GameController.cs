be7a4d32319904cc268197300da725f0e9d5170b
Nathaniel Richards <nathrich23@csu.fullerton.edu>
2020-02-11 01:46:06 -0800

    Main menu and Plague Panic Setup

---
+++ CavalcadeGame/Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameController
+{
+    public static float Timer { get; set; }
+    public static int Score { get; set; }
+    public static void UpdateTier()
+    {
+        Timer -= Time.deltaTime;
+    }
+    public static void UpdateScore(int points)
+    {
+        Score += points;
+    }
+}
---
+++ CavalcadeGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        GameController.Timer = 60.0f;
+        GameController.Score = 0;
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+
+    }
+}
